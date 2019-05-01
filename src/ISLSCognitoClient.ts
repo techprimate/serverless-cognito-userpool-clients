d03d7dc90b0babf64fcd322ac9e5eb5cfca5387c
Philip Niedertscheider <phil@techprimate.com>
2019-05-01 13:08:42 +0200

    Added more typings and domain management

---
+++ src/ISLSCognitoClient.ts
@@ -0,0 +1,15 @@
+export interface ISLSCognitoClient {
+    userPoolId: string;
+    clientId: string;
+    callbackUrls?: string[];
+    logoutUrls?: string[];
+    allowedOAuthFlows?: string[];
+    allowedOAuthScopes?: string[];
+    allowedOAuthFlowsUserPoolClient?: boolean;
+    supportedIdentityProviders?: string[];
+
+    customDomain?: {
+        name: string,
+        certificateArn: string,
+    };
+}
--- src/plugin.ts
+++ src/plugin.ts
@@ -1,63 +1,172 @@
+import { CognitoIdentityServiceProvider } from 'aws-sdk';
+import { Serverless } from '../typings/serverless/index';
+import { ISLSCognitoClient } from './ISLSCognitoClient';
+
 export class CognitoClientsPlugin {
-    public hooks: {};
-    public provider: Serverless.Provider.Aws;
-    public commands: {};
+  public hooks: {};
+  public provider: Serverless.Provider.Aws;
+  public commands: {};
 
-    constructor(private serverless: Serverless) {
-        this.hooks = {
-            'after:deploy:deploy': this.process.bind(this),
-            'cognito_clients:deploy:deploy': this.process.bind(this),
-        };
-        this.provider = this.serverless.getProvider('aws');
-        this.commands = {
-            cognito_clients: {
-                commands: {
-                    deploy: {
-                        lifecycleEvents: [
-                            'deploy',
-                        ],
-                        usage: 'Deploys a domain using the domain name defined in the serverless file',
-                    },
-                },
-                lifecycleEvents: [
-                    'deploy',
-                ],
-            },
-        };
+  constructor(private serverless: Serverless) {
+    this.hooks = {
+      'after:deploy:deploy': this.process.bind(this),
+      'cognito_clients:deploy:deploy': this.process.bind(this),
+    };
+    this.provider = this.serverless.getProvider('aws');
+    this.commands = {
+      cognito_clients: {
+        commands: {
+          deploy: {
+            lifecycleEvents: [
+              'deploy',
+            ],
+            usage: 'Deploys a domain using the domain name defined in the serverless file',
+          },
+        },
+        lifecycleEvents: [
+          'deploy',
+        ],
+      },
+    };
+  }
+
+  private async process() {
+    const custom = this.serverless.service.custom;
+    if (!custom) {
+      return;
+    }
+    const configs = this.serverless.service.custom.cognitoClients;
+    if (!configs || !Array.isArray(configs)) {
+      return;
     }
 
-    private async process() {
-        const custom = this.serverless.service.custom;
-        if (!custom) {
-            return;
-        }
-        const configs = this.serverless.service.custom.cognitoClients;
-        if (!configs || !Array.isArray(configs)) {
-            return;
-        }
-
-        for (const config of configs) {
-            const params = {
-                AllowedOAuthFlows: config.allowedOAuthFlows,
-                AllowedOAuthFlowsUserPoolClient: config.allowedOAuthFlowsUserPoolClient,
-                AllowedOAuthScopes: config.allowedOAuthScopes,
-                CallbackURLs: config.callbackUrls,
-                ClientId: config.clientId,
-                LogoutURLs: config.logoutUrls,
-                SupportedIdentityProviders: config.supportedIdentityProviders,
-                UserPoolId: config.userPoolId,
-            };
-            const result = await this.provider.request(
-                'CognitoIdentityServiceProvider',
-                'updateUserPoolClient',
-                params,
-                this.serverless.getProvider('aws').getStage(),
-                this.serverless.getProvider('aws').getRegion(),
-            );
-            if (result.error) {
-                this.serverless.cli.log('Updated Cognito User Pool Client: ' + config.clientId);
-            }
-            this.serverless.cli.log('Updated Cognito User Pool Client: ' + config.clientId);
-        }
+    for (const config of configs) {
+      await this.updateClient(config);
+      await this.updateDomain(config);
     }
+  }
+
+  private async updateClient(config: ISLSCognitoClient) {
+    const params = {
+      AllowedOAuthFlows: config.allowedOAuthFlows,
+      AllowedOAuthFlowsUserPoolClient: config.allowedOAuthFlowsUserPoolClient,
+      AllowedOAuthScopes: config.allowedOAuthScopes,
+      CallbackURLs: config.callbackUrls,
+      ClientId: config.clientId,
+      LogoutURLs: config.logoutUrls,
+      SupportedIdentityProviders: config.supportedIdentityProviders,
+      UserPoolId: config.userPoolId,
+    };
+    const result = await this.provider.request(
+      'CognitoIdentityServiceProvider',
+      'updateUserPoolClient',
+      params,
+      this.serverless.getProvider('aws').getStage(),
+      this.serverless.getProvider('aws').getRegion(),
+    );
+    if (result.error) {
+      this.serverless.cli.log('Updated Cognito User Pool Client: ' + config.clientId);
+    }
+    this.serverless.cli.log('Updated Cognito User Pool Client: ' + config.clientId);
+  }
+
+  private async updateDomain(config: ISLSCognitoClient) {
+    const userPool = await this.describeUserPool(config.userPoolId);
+    if (!userPool.CustomDomain && !config.customDomain) { // no domain created nor given
+      return;
+    }
+    if (userPool.CustomDomain && !config.customDomain) { // Delete remote
+      await this.deleteUserPoolDomain(userPool.Id, userPool.CustomDomain);
+    } else if (config.customDomain && !userPool.CustomDomain) { // Create remote
+      await this.createUserPoolDomain(config.userPoolId, config.customDomain.name, config.customDomain.certificateArn);
+    } else { // Update remote
+      if (userPool.CustomDomain !== config.customDomain.name) {
+        await this.deleteUserPoolDomain(userPool.Id, userPool.CustomDomain);
+        await this.createUserPoolDomain(config.userPoolId,
+          config.customDomain.name,
+          config.customDomain.certificateArn);
+      } else {
+        const userPoolDomain = await this.describeUserPoolDomain(config.userPoolId);
+        if (userPoolDomain.CustomDomainConfig.CertificateArn !== config.customDomain.certificateArn) {
+          await this.updateUserPoolDomain(config.userPoolId, config.customDomain.name, config.customDomain.certificateArn);
+        }
+      }
+    }
+  }
+
+  private async describeUserPool(id: string): Promise<CognitoIdentityServiceProvider.Types.UserPoolType> {
+    const params: CognitoIdentityServiceProvider.Types.DescribeUserPoolRequest = {
+      UserPoolId: id,
+    };
+    const result = await this.provider.request(
+      'CognitoIdentityServiceProvider',
+      'describeUserPool',
+      params,
+      this.serverless.getProvider('aws').getStage(),
+      this.serverless.getProvider('aws').getRegion(),
+    );
+    return result.UserPool;
+  }
+
+  private async createUserPoolDomain(userPoolId: string, domain: string, certificateArn: string) {
+    const params: CognitoIdentityServiceProvider.Types.CreateUserPoolDomainRequest = {
+      CustomDomainConfig: {
+        CertificateArn: certificateArn,
+      },
+      Domain: domain,
+      UserPoolId: userPoolId,
+    };
+    await this.provider.request(
+      'CognitoIdentityServiceProvider',
+      'createUserPoolDomain',
+      params,
+      this.serverless.getProvider('aws').getStage(),
+      this.serverless.getProvider('aws').getRegion(),
+    );
+  }
+
+  private async deleteUserPoolDomain(userPoolId: string, domain: string) {
+    const params: CognitoIdentityServiceProvider.Types.DeleteUserPoolDomainRequest = {
+      Domain: domain,
+      UserPoolId: userPoolId,
+    };
+    await this.provider.request(
+      'CognitoIdentityServiceProvider',
+      'deleteUserPoolDomain',
+      params,
+      this.serverless.getProvider('aws').getStage(),
+      this.serverless.getProvider('aws').getRegion(),
+    );
+  }
+
+  private async updateUserPoolDomain(userPoolId: string, domain: string, certificate: string) {
+    const params: CognitoIdentityServiceProvider.Types.UpdateUserPoolDomainRequest = {
+      CustomDomainConfig: {
+        CertificateArn: certificate,
+      },
+      Domain: domain,
+      UserPoolId: userPoolId,
+    };
+    await this.provider.request(
+      'CognitoIdentityServiceProvider',
+      'deleteUserPoolDomain',
+      params,
+      this.serverless.getProvider('aws').getStage(),
+      this.serverless.getProvider('aws').getRegion(),
+    );
+  }
+
+  private async describeUserPoolDomain(domain: string): Promise<CognitoIdentityServiceProvider.DomainDescriptionType> {
+    const params: CognitoIdentityServiceProvider.Types.DescribeUserPoolDomainRequest = {
+      Domain: domain,
+    };
+    const result: CognitoIdentityServiceProvider.Types.DescribeUserPoolDomainResponse = await this.provider.request(
+      'CognitoIdentityServiceProvider',
+      'describeUserPoolDomain',
+      params,
+      this.serverless.getProvider('aws').getStage(),
+      this.serverless.getProvider('aws').getRegion(),
+    );
+    return result.DomainDescription;
+  }
 }
